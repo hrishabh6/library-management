9f87ad0bf8aa8067e1b446b8986ff194b17027b3
hrishabh6 <hrishabhjoshi123@gmail.com>
2025-01-18 20:20:26 +0530

    Qstash setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,8 +7,7 @@ import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 
 const Home = async () => {
-  const result = await db.select().from(users)
-  console.log(JSON.stringify(result, null, 2))
+  
   return (
     <>
       {/*  @ts-ignore */}

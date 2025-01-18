b8ed57bb6f48c59d74ec298b32cda33937d06b00
hrishabh6 <hrishabhjoshi123@gmail.com>
2025-01-18 19:47:35 +0530

    redis setup/rate limiting implemented

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <main className='root-container flex min-h-screen flex-col items-center justify-center'>
+            <h1 className="text-5xl max-sm:text-4xl font-bebas-neue font-bold mb-4 text-white">Whoa, slow down there, speedster!🚦</h1>
+            <p className="text-lg mb-4 max-w-xl text-center text-light-400">
+                Looks like you've hit the brakes on our speed limit 🚀. Take a chill pill, grab a snack 🍕, and try again in a few.
+            </p>
+            <p className="text-lg mb-6 text-white">
+                Don’t worry, we’re still here for you—just not all at once 😉.
+            </p>
+
+        </main>
+    )
+}
+
+export default page
